57cb21f4b7e73151efc159dfd5fea6a8b94a8de3
mangekyo443556 <73116083+mangekyo443556@users.noreply.github.com>
2025-04-25 01:07:15 +0000

    add new dags

---
+++ dags/parallelims_example.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+with DAG(
+    dag_id='ejemplo_2_paralelo',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False
+) as dag:
+    inicio = EmptyOperator(task_id='inicio')
+    tarea_1 = EmptyOperator(task_id='tarea_1')
+    tarea_2 = EmptyOperator(task_id='tarea_2')
+    fin = EmptyOperator(task_id='fin')
+
+    inicio >> [tarea_1, tarea_2] >> fin
